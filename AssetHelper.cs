6a9df162382deb66d0a2a376f2e9951e7d06f4d3
Atokal <82206645+Atokal@users.noreply.github.com>
2021-04-09 03:25:17 -0500

    Add files via upload

---
+++ AssetHelper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace AtosArrows
+{
+    class AssetHelper
+    {
+        public static Items.RecipesConfig recipes;
+        public static AssetBundle assetBundle;
+        public static void Init(string assetbundle, string recipejson)
+        {
+            assetBundle = Tools.LoadAssetBundle(assetbundle);
+            recipes = Tools.LoadJsonFile<Items.RecipesConfig>(recipejson);
+
+            foreach (var recipe in recipes.recipes)
+            {
+                if (recipe.enabled)
+                {
+                    if (assetBundle.Contains(recipe.item))
+                    {
+                        var prefab = assetBundle.LoadAsset<GameObject>(recipe.item);
+                        PrefabHelper.AddCustomItem(prefab, recipes);
+                        PrefabHelper.AddCustomRecipe(prefab, recipes);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AtosArrows.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using System.Reflection;
+using UnityEngine;
+using HarmonyLib;
+using ValheimLib;
+using ValheimLib.ODB;
+
+
+
+namespace AtosArrows
+{
+    //BepInPlugin - GUID/Name/Version
+    //BepinDependency - what mod is dependant. for importing assets from unity use valheim.lib.valheim.lib.moguid
+    [BepInPlugin("AtoArrows", "Atos Arrows", "0.1")]
+    [BepInDependency(ValheimLib.ValheimLib.ModGuid)]
+
+    //Makes it so its an exntension of base unity plugin. 
+    public class AtoSword : BaseUnityPlugin
+    {
+        // private = only Harmony can access _harmoney, making it a private variable. 
+        private Harmony _harmony;
+        //internal pointer for my class to get and set it to private
+        internal static AtoSword Instance { get; private set; }
+
+        private void Awake()
+        {
+            Instance = this;
+            AssetHelper.Init("atoarrows", "Atos_Arrows.json");
+            this._harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+        }
+    }
+}
